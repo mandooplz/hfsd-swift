ee4c377666b5560199dbf6fb860faac99cb6c626
kmw4561@gmail.com <kmw4561@gmail.com>
2025-12-08 17:03:13 +0900

    [CHORE] QuickSorter 파일 생성

---
+++ 06-solver/Sources/Solver/QuickSorter/QuickSorter.swift
@@ -0,0 +1,20 @@
+//
+//  QuickSorter.swift
+//  Solver
+//
+//  Created by 김민우 on 12/8/25.
+//
+import Foundation
+
+
+// MARK: Object
+@MainActor
+public final class QuickSorter: Sendable {
+    // MARK: core
+    
+    
+    // MARK: state
+    
+    
+    // MARK: action
+}
--- 06-solver/Tests/SolverTests/BubbleSorter/BubbleSorterTests.swift
+++ 06-solver/Tests/SolverTests/BubbleSorter/BubbleSorterTests.swift
@@ -4,4 +4,15 @@
 //
 //  Created by 김민우 on 12/8/25.
 //
+import Foundation
+import Testing
+import Solver
 
+
+// MARK: Tests
+@Suite
+struct BubbleSorterTests {
+    @Test func <#test name#>() async throws {
+        
+    }
+}
