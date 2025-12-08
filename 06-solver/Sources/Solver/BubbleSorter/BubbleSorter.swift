4e364736f246619fcfaf2d52479122091f6da44f
kmw4561@gmail.com <kmw4561@gmail.com>
2025-12-08 23:41:16 +0900

    [REFACTOR] BubbleSorter 관련 코드 주석 처리

--- 06-solver/Sources/Solver/BubbleSorter/BubbleSorter.swift
+++ 06-solver/Sources/Solver/BubbleSorter/BubbleSorter.swift
@@ -1,63 +1,63 @@
+////
+////  BubbleSorter.swift
+////  Solver
+////
+////  Created by 김민우 on 12/8/25.
+////
+//import Foundation
 //
-//  BubbleSorter.swift
-//  Solver
 //
-//  Created by 김민우 on 12/8/25.
-//
-import Foundation
-
-
-// MARK: Object
-@MainActor
-public final class BubbleSorter: Sendable {
-    // MARK: core
-    public init(data: [Int]) {
-        self.data = data
-    }
-    
-    
-    // MARK: state
-    private nonisolated let data: [Int]
-    
-    public private(set) var items: [BubbleItem] = []
-    public func getValidItems() -> [BubbleItem] {
-        self.items.filter {
-            $0.isConfirmed == false
-        }
-    }
-    internal func getItem(_ index: Int) -> BubbleItem? {
-        guard self.items.indices.contains(index) else {
-            return nil
-        }
-        
-        return self.items[index]
-    }
-    public func getResult() -> [Int] {
-        self.items.map {
-            $0.value
-        }
-    }
-    
-    
-    
-    // MARK: action
-    public func setUp() {
-        // capture
-        guard self.items.isEmpty == true else {
-            print("BubbleSorter.items에 아이템들이 이미 존재합니다.")
-            return
-        }
-        
-        // process
-        let enumeratedData = data.enumerated()
-        
-        // mutate
-        self.items = enumeratedData.map { index, num in
-            BubbleItem(
-                owner: self,
-                index: index,
-                value: num
-            )
-        }
-    }
-}
+//// MARK: Object
+//@MainActor
+//public final class BubbleSorter: Sendable {
+//    // MARK: core
+//    public init(data: [Int]) {
+//        self.data = data
+//    }
+//    
+//    
+//    // MARK: state
+//    private nonisolated let data: [Int]
+//    
+//    public private(set) var items: [BubbleItem] = []
+//    public func getValidItems() -> [BubbleItem] {
+//        self.items.filter {
+//            $0.isConfirmed == false
+//        }
+//    }
+//    internal func getItem(_ index: Int) -> BubbleItem? {
+//        guard self.items.indices.contains(index) else {
+//            return nil
+//        }
+//        
+//        return self.items[index]
+//    }
+//    public func getResult() -> [Int] {
+//        self.items.map {
+//            $0.value
+//        }
+//    }
+//    
+//    
+//    
+//    // MARK: action
+//    public func setUp() {
+//        // capture
+//        guard self.items.isEmpty == true else {
+//            print("BubbleSorter.items에 아이템들이 이미 존재합니다.")
+//            return
+//        }
+//        
+//        // process
+//        let enumeratedData = data.enumerated()
+//        
+//        // mutate
+//        self.items = enumeratedData.map { index, num in
+//            BubbleItem(
+//                owner: self,
+//                index: index,
+//                value: num
+//            )
+//        }
+//    }
+//}
