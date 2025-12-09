7e662c13be8199f0501d63a6048b1ca0d96ce697
kmw4561@gmail.com <kmw4561@gmail.com>
2025-12-09 22:13:37 +0900

    [REFACTOR] TicTacToeView 리팩토링

--- 03-tictactoe-mac/TicTacToeMac/Presentation/TicTacToeView.swift
+++ 03-tictactoe-mac/TicTacToeMac/Presentation/TicTacToeView.swift
@@ -22,41 +22,50 @@ struct TicTacToeView: View {
     
     // MARK: body
     var body: some View {
-        NavigationSplitView {
-            NewGameButton(
-                label: "New Game",
-                action: {
-                    Task {
-                        await tictactoeRef.createGame()
-                        
-                        self.selectedBoard = tictactoeRef.boardList.last
+        TicTacToeLayout(
+            sidebar: {
+                NewGameButton(
+                    label: "New Game",
+                    action: {
+                        Task {
+                            await tictactoeRef.createGame()
+                            
+                            self.selectedBoard = tictactoeRef.boardList.last
+                        }
                     }
-                }
-            )
-            
-            GameList
-            
-        } detail: {
-            if let selectedBoard,
-                let board = selectedBoard.ref {
-                GameBoardView(board)
-            } else {
-                VStack {
-                    Image(systemName: "gamecontroller")
-                        .font(.largeTitle)
-                    Text("새로운 게임을 시작해보세요")
-                        .font(.title)
-                    Text("New Game 버튼을 눌러 새로운 게임을 시작할 수 있습니다.")
-                        .font(.subheadline)
-                        .foregroundStyle(.secondary)
+                )
+                
+                GameList(
+                    tictactoeRef: tictactoeRef,
+                    selectedBoard: $selectedBoard
+                )
+            },
+            content: {
+                if let selectedBoard,
+                    let board = selectedBoard.ref {
+                    GameBoardView(board)
+                } else {
+                    BoardEmptyView()
                 }
             }
-        }
+        )
     }
 }
 
 
 // MARK: Component
+fileprivate struct TicTacToeLayout<S: View, M: View> : View {
+    @ViewBuilder let sidebar: () -> S
+    @ViewBuilder let content: () -> M
+    
+    var body: some View {
+        NavigationSplitView {
+            sidebar()
+        } detail: {
+            content()
+        }
+    }
+}
 fileprivate struct NewGameButton: View {
     let label: String
     let action: () -> Void
@@ -69,10 +78,12 @@ fileprivate struct NewGameButton: View {
     }
 }
 
-extension TicTacToeView {
+fileprivate struct GameList: View {
+    let tictactoeRef: TicTacToe
+    let selectedBoard: Binding<GameBoard.ID?>
     
-    var GameList: some View {
-        List(tictactoeRef.boardList, selection: $selectedBoard) { gameBoardID in
+    var body: some View {
+        List(tictactoeRef.boardList, selection: selectedBoard) { gameBoardID in
             NavigationLink(value: gameBoardID) {
                 if let gameBoardRef = gameBoardID.ref {
                     GameBoardLabel(gameBoardRef)
@@ -81,6 +92,21 @@ extension TicTacToeView {
         }
     }
 }
+
+fileprivate struct BoardEmptyView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "gamecontroller")
+                .font(.largeTitle)
+            Text("새로운 게임을 시작해보세요")
+                .font(.title)
+            Text("New Game 버튼을 눌러 새로운 게임을 시작할 수 있습니다.")
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+        }
+    }
+}
+
 
 
 // Preview
